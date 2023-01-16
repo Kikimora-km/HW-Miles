359d2eb718c280e1f120671e6b7a6b735bdbd354
Saidova Linda <kikimora.99.km@gmail.com>
2023-01-16 22:15:53 +0300

    BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 20_000;
-
-        int bonus = price/20;
-
-        System.out.println("Bonus " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
