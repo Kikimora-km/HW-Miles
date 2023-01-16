612bf8b509b582e0bd76a3632e0bc7a5a1e27a73
Saidova Linda <kikimora.99.km@gmail.com>
2023-01-16 12:05:51 +0300

    add code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20_000;
+
+        int bonus = price/20;
+
+        System.out.println("Bonus " + bonus);
+    }
+}
